b1af519523112fce6766d523168882e3aa76d0dc
Anselmo Polcaro Ribeiro <66381597+polcaronet@users.noreply.github.com>
2024-05-03 23:23:34 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,6 @@
+# _`Portifolio`_ do `Anselmo Polcaro` Dev ⚙ 
+**Projeto top da construção do meu `Portifolio online!`**<br>
+##
+**_`Meu site`_:**</div>
+<a href="https://portifolio-five-jet.vercel.app/" target="_blank"><img align="left" alt="Ansel-pic" height="30" style="border-radius:30px;" src="https://user-images.githubusercontent.com/66381597/167222900-88b7923c-a06d-46d4-bd88-8ed2cb883f7d.png" target="_blank">  **Portifolio** </a>
+
---
+++ script.js
@@ -0,0 +1,63 @@
+  
+  AOS.init();
+
+// Script slides
+
+let count = 1;
+document.getElementById("radio1").checked = true;
+
+setInterval( function(){
+  nextImage();
+}, 7000)
+
+function nextImage(){
+  count++;
+  if(count > 9){
+     count = 0;
+  }
+
+  document.getElementById("radio"+count).checked = true;
+}
+
+// Script typing
+
+
+var typingTime = document.getElementById('typing');
+typing.innerHTML = '';
+var count1 = 0;
+var str ='🎓 student e sou desenvolvedor Full Stack!';
+var typingTime = setInterval(function() {
+  count1 = count1 + 1;
+  typing.innerHTML = " " + str.slice(0, count1);
+  if (count1 === str.length) {
+    clearInterval(typing);
+    typing.innerHTML = " " + str
+    count1 = 0;
+    setInterval(function() {
+
+      if (count1 === 0) {
+        typing.innerHTML = " " + str + "|"
+        count1 = 1;
+      } else {
+        typing.innerHTML = " " + str
+        count1 = 0;
+      };
+    }, 10000000);
+  };
+}, 130)
+
+// MenuShow
+
+function menuShow() {
+  let menuMobile = document.querySelector('.mobile-menu');
+  if (menuMobile.classList.contains('open')) {
+      menuMobile.classList.remove('open');
+      document.querySelector('.icon').src = "assets/menu_white_36dp.svg";
+  } else {
+      menuMobile.classList.add('open');
+      document.querySelector('.icon').src = "assets/close_white_36dp.svg";
+  }
+}
+
+
+   
